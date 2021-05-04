f8e8723f0b25dbd3229b6855bc1474185607994b
ziga <ziga.rojec@fe.uni-lj.si>
2021-05-04 21:07:08 +0200

    Completely new scorefunction definition for commonEmitter amplifyer. UTILIZING MODEL SEQUENCING based on MODELSCHEME.

--- scoreFunctions.py
+++ scoreFunctions.py
@@ -28,6 +28,7 @@ debug = 0
 
 # TODO Make every scoreFunction a single file. This is getting to damn big!!
 
+ 
 def scoreCirc_commonEmitterAmp_resilenceMode(circuit, gen, indi, MOEAMODE):
     """
     # Slice the current scoreFunction into cheching the BigCircuitMatrix here
@@ -36,8 +37,6 @@ def scoreCirc_commonEmitterAmp_resilenceMode(circuit, gen, indi, MOEAMODE):
     # There is no need to complicate actually.
     # Just create an interator in the else statement in scorecirc that would work with or without existing modelScheme...
     """
-    
-    #print(MODELSCHEME)
     
     if debug > 2:
         print("\t\tG_" + str(gen) + "_I_" + str(indi))
@@ -59,67 +58,87 @@ def scoreCirc_commonEmitterAmp_resilenceMode(circuit, gen, indi, MOEAMODE):
     else:
         # Create netlist and calculate the cost function. 
         # Based on all models that are being used, create a series of evaluations, using the models listed in MODELSCHEME!
+        if robustMode:
+            results_list = []
+            for elm in MODELSCHEME:
+                elm_num = elm.split("_")
+                for mod in MODELSCHEME[elm]:
+                    #print("\t", mod, elm_num[0], elm_num[1])
+                    # TODO Solve the repeated nominal model analysis!
+                    makeNetlist_netlister(circuit, 
+                                          Element = elm_num[0], 
+                                          ElementNo =  int(elm_num[1]),
+                                          ModelName = mod)
+                    
+
+                    #makeNetlist_netlister(circuit)
+                    results = runme2.evaluate_CommonEmitterAmp(circuit.filename)
+                    results_list.append(results)
+                    #print(results)
+                    disfCount = 0
+                    dcgain = np.array(results['DCgain']['nominal'], dtype=float)
+                    if np.isnan(dcgain):
+                        disfCount += 1
+                        _dcgain = 0      
+                    else:
+                        _dcgain = abs(dcgain - 4.8e3) if dcgain < 4.8e3 else 0            # Nominal value deducted. # TODO DECIBELS!!
+                    
+                    dcvout_rmse = np.array(results['dcvout_rmse']['nominal'], dtype=float)
+                    if np.isnan(dcvout_rmse):
+                        disfCount += 1
+                        _dcvout_rmse = 0      
+                    else:
+                        _dcvout_rmse = abs(dcvout_rmse-7.6e-3)        # Nominal value deducted. 
+                    
+                    maxpower = np.array(results['maxpower']['nominal'], dtype=float)
+                    if np.isnan(maxpower):
+                        disfCount += 1
+                        _maxpower = 0      
+                    else:
+                        _maxpower = abs(maxpower-(7.1e-3))          # Nominal value deducted. 
+                
+                    gain_stddev_norm = np.array(results['gain_stddev_norm']['nominal'], dtype=float)
+                    if np.isnan(gain_stddev_norm):
+                        disfCount += 1
+                        _gain_stddev_norm = 0      
+                    else:   # If stddev lower than 1% it does not count. 
+                        _gain_stddev_norm = abs(gain_stddev_norm) if gain_stddev_norm > 0.01 else 0   
+                     
+
+                    #----------------------------------------------------------Score function SINGLE-OBJECTIVE
+                    if MOEAMODE == 0:
+                        score +=  sum([_dcgain, _dcvout_rmse, _maxpower, _gain_stddev_norm])
+
+                        if disfCount > 0:
+                            score = 0 + np.exp(disfCount) * 1e3 + random.random()*10
+                        
+                        if debug > 2:
+                            print(_dcgain, _dcvout_rmse, _maxpower, _gain_stddev_norm)
+                    
+                    #----------------------------------------------------------Score function MULTI-OBJECTIVE
+                    else: #MOEAMODE == 1
+                        score = np.array([_dcgain, _maxpower, _gain_stddev_norm]) + 0 # + _dcvout_rmse
+                        if disfCount > 0:
+                            score = (np.array([0,0,0])+np.exp(disfCount) * 1e3) + np.array([random.random()*10,random.random()*10,random.random()*10])
+                        
+                        #print "score64", score
+                        if gen > 30:
+                            for i in range(0, len(score)): #round to 5 decimal points
+                                score[i] = np.float16(score[i]) 
+                            #print "score16", score
+                        score = np.ndarray.tolist(score) #for faster non-dominant sorting let the score be python array instead of np.array 5.7.2017
+                    #-------------------------------------------------------------------
+                    if debug > 2:    
+                        print("\t\tG_" + str(gen) + "_I_" + str(indi) + " SCORE:", score)
+                        
+                    #input() # Wait and inspect every netlist please PROCEED AND TEST HERE
+                    os.remove(circuit.filename) #cleanup current subcircuit
             
-        makeNetlist_netlister(circuit)
-        results = runme2.evaluate_CommonEmitterAmp(circuit.filename)
+            return score, results_list
         
-   
-        dcgain = np.array(results['DCgain']['nominal'], dtype=float)
-        if np.isnan(dcgain):
-            disfCount += 1
-            _dcgain = 0      
         else:
-            _dcgain = abs(dcgain - 4.8e3)             # Nominal value deducted. 
-        
-        dcvout_rmse = np.array(results['dcvout_rmse']['nominal'], dtype=float)
-        if np.isnan(dcvout_rmse):
-            disfCount += 1
-            _dcvout_rmse = 0      
-        else:
-            _dcvout_rmse = abs(dcvout_rmse-7.6e-3)        # Nominal value deducted. 
-        
-        maxpower = np.array(results['maxpower']['nominal'], dtype=float)
-        if np.isnan(maxpower):
-            disfCount += 1
-            _maxpower = 0      
-        else:
-            _maxpower = abs(maxpower-(7.1e-3))          # Nominal value deducted. 
-    
-        gain_stddev_norm = np.array(results['gain_stddev_norm']['nominal'], dtype=float)
-        if np.isnan(gain_stddev_norm):
-            disfCount += 1
-            _gain_stddev_norm = 0      
-        else:   # If stddev lower than 1% it does not count. 
-            _gain_stddev_norm = abs(gain_stddev_norm) if gain_stddev_norm > 0.01 else 0   
-        
-        if debug > 2:      
-            print(_dcgain, _dcvout_rmse, _maxpower, _gain_stddev_norm)
-
-
-        #----------------------------------------------------------Score function SINGLE-OBJECTIVE
-        if MOEAMODE == 0:
-            score =  sum([_dcgain, _dcvout_rmse, _maxpower, _gain_stddev_norm])
-
-            if disfCount > 0:
-                score = 0 + np.exp(disfCount) * 1e3 + random.random()*10
-        
-        #----------------------------------------------------------Score function MULTI-OBJECTIVE
-        else: #MOEAMODE == 1
-            score = np.array([_dcgain, _maxpower, _gain_stddev_norm]) + 0 # + _dcvout_rmse
-            if disfCount > 0:
-                score = (np.array([0,0,0])+np.exp(disfCount) * 1e3) + np.array([random.random()*10,random.random()*10,random.random()*10])
-            
-            #print "score64", score
-            if gen > 30:
-                for i in range(0, len(score)): #round to 5 decimal points
-                    score[i] = np.float16(score[i]) 
-                #print "score16", score
-            score = np.ndarray.tolist(score) #for faster non-dominant sorting let the score be python array instead of np.array 5.7.2017
-        #-------------------------------------------------------------------
-        if debug > 2:    
-            print("\t\tG_" + str(gen) + "_I_" + str(indi) + " SCORE:", score)
-
-    os.remove(circuit.filename) #cleanup current subcircuit
+            print("TODO!")
+            # What TODO when no robustMode ... 
 
     return score, results
 
